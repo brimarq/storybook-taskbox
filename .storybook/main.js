6f9c83546370c6c7d02ab53ff92ed4f40c9de369
Brian Marquis <rbmarquis.dev@gmail.com>
2020-09-07 16:48:00 -0500

    adjust storybook config & add css

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,19 @@
+// module.exports = {
+//   "stories": [
+//     "../src/**/*.stories.mdx",
+//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/preset-create-react-app"
+//   ]
+// }
+
+// Reconfigure to notice only stories in src/components
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/components/**/*.stories.js"
   ],
   "addons": [
     "@storybook/addon-links",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import '../src/index.css';
 
+/**
+ * `parameters` are typically used to control the behavior of Storybook's features and addons. 
+ * In our case we're going to use them to configure how the actions (mocked callbacks) are handled. 
+ * `actions` allows us to create callbacks that appear in the actions panel of the Storybook UI when clicked. 
+ * So when we build a pin button, we’ll be able to determine in the test UI if a button click is successful.
+ */
+
+// Configures Storybook to log the actions(onArchiveTask and onPinTask) in the UI.
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
