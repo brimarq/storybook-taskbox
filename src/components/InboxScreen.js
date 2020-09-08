7a811fb99b441da0a16fb1cca10bc89b42a969f2
Brian Marquis <rbmarquis.dev@gmail.com>
2020-09-08 16:47:03 -0500

    add mock redux store with decorator

--- src/components/InboxScreen.js
+++ src/components/InboxScreen.js
@@ -23,7 +23,7 @@ export function PureInboxScreen({error}) {
   }
 
   return (
-    <div className="page-lists-show">
+    <div className="page lists-show">
       <nav>
         <h1 className="title-page">
           <span className="title-wrapper">Taskbox</span>
