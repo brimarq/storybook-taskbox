7a855aa7ac6704b5d502caa045b6ee8bb36c912c
Brian Marquis <rbmarquis.dev@gmail.com>
2020-09-13 17:48:13 -0500

    create new addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   "stories": [
-//     "../src/**/*.stories.mdx",
-//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/preset-create-react-app"
-//   ]
-// }
-
-// Reconfigure to notice only stories in src/components
 module.exports = {
   "stories": [
     "../src/components/**/*.stories.js"
@@ -18,6 +5,7 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    "./design-addon/register.js"
   ]
 }
